93ddc47469972aae287f6c0d6289e69f60300f81
ksilin <konstantin.silin@gmail.com>
2014-08-07 20:45:01 +0200

    fixed colors

--- public/js/6_bar_chart.js
+++ public/js/6_bar_chart.js
@@ -1,12 +1,12 @@
 var dataset = [];
 
 for (var i = 0; i < 25; i++) {            //Loop 25 times
-    var newNumber = Math.floor(Math.random() * 30);   //New random number (0-30)
+    var newNumber = 5 + Math.floor(Math.random() * 30);   //New random number (0-30)
     dataset.push(newNumber);              //Add new number to array
 }
 
 var w = 500;
-var h = 100;
+var h = 200;
 
 var barPadding = 1;
 
@@ -29,10 +29,23 @@ bars.attr("x", function (d, i) {
         return d * 4;
     })
     .attr("width", w / dataset.length - barPadding)
-    .attr("fill", function(d) {
-        return "rgb(0, 0, " + (d * 10) + ")";
+    .attr("fill", function (d) {
+        return "rgb(20, 20, " + (d * 8) + 50 + ")";
     });
-//    .attr("stroke", "orange")
-//    .attr("stroke-width", function (d) {
-//        return 1;
-//    });
+
+svg.selectAll('text')
+    .data(dataset)
+    .enter()
+    .append('text')
+    .text(function (d) {
+        return d;
+    })
+    .attr('fill', 'white')
+    .attr("x", function(d, i) {
+        return i * (w / dataset.length) + 4;
+    })
+    .attr("font-family", "sans-serif")
+    .attr("font-size", "11px")
+    .attr("y", function(d) {
+        return h - (d * 4) + 15;
+    });
