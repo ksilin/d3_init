30412f80dad3df27bf0dea475f01667446348d09
ksilin <konstantin.silin@gmail.com>
2014-08-14 21:27:58 +0200

    added reset with transitions to bar chart

--- public/js/9_bar_chart.js
+++ public/js/9_bar_chart.js
@@ -1,9 +1,15 @@
 var dataset = [];
 
-for (var i = 0; i < 25; i++) {            //Loop 25 times
-    var newNumber = 5 + Math.floor(Math.random() * 30);   //New random number (0-30)
-    dataset.push(newNumber);              //Add new number to array
+var refresh_data = function (dataset) {
+    dataset = []
+    for (var i = 0; i < 25; i++) {            //Loop 25 times
+        var newNumber = 5 + Math.floor(Math.random() * 30);   //New random number (0-30)
+        dataset.push(newNumber);              //Add new number to array
+    }
+    return dataset;
 }
+
+dataset = refresh_data(dataset);
 
 var low = d3.min(dataset);
 var high = d3.max(dataset);
@@ -20,6 +26,10 @@ var xScale = d3.scale.ordinal()
     .domain(d3.range(dataset.length))
     .rangeRoundBands([0, w], 0.05);
 
+var yScale = d3.scale.linear()
+    .domain([0, d3.max(dataset)])
+    .range([0, h]);
+
 //separating bars
 var barPadding = 1;
 
@@ -34,12 +44,12 @@ var bars = svg.selectAll("rect")
 
 bars.attr("x", function (d, i) {
     return xScale(i);
-    })
+})
     .attr("y", function (d) {
-        return h - (d * 4);
+        return h - yScale(d);
     })
     .attr("height", function (d) {
-        return d * 4;
+        return yScale(d);
     })
     .attr("width", xScale.rangeBand())
     .attr("fill", function (d) {
@@ -59,10 +69,39 @@ svg.selectAll('text')
     })
     .attr('fill', 'white')
     .attr("x", function (d, i) {
-        return xScale(i) + 5;
+        return xScale(i) + xScale.rangeBand() / 2 - 6;
+    })
+    .attr("y", function (d) {
+        return h - yScale(d) + 14;
     })
     .attr("font-family", "sans-serif")
-    .attr("font-size", "11px")
-    .attr("y", function (d) {
-        return h - (d * 4) + 15;
-    });
+    .attr("font-size", "11px");
+
+d3.select('p').on('click', function () {
+    dataset = refresh_data(dataset)
+    svg.selectAll("rect")
+        .data(dataset)
+        .transition()
+        .attr("y", function (d) {
+            return h - yScale(d);
+        })
+        .attr("fill", function (d) {
+            return "rgb(10, " + (59 + Math.floor(192 * norm(d))) + ", " + (72 + Math.floor(170 * norm(d))) + ")";
+        })
+        .attr("height", function (d) {
+            return yScale(d);
+        });
+
+    svg.selectAll("text")
+        .data(dataset)
+        .transition()
+        .text(function (d) {
+            return d;
+        })
+        .attr("x", function (d, i) {
+            return xScale(i) + xScale.rangeBand() / 2 - 6;
+        })
+        .attr("y", function (d) {
+            return h - yScale(d) + 14;
+        });
+})
